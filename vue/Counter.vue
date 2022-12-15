accfaa30757f321c98467738a843469d25e6e272
Aleix Carles <aleixcarles@iesebre.com>
2022-12-15 16:45:30 +0100

    Versió SIngle FIle Components

---
+++ vue/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement()">-</button>
+  </div>
+</template>
+<script>
+module.exports = {
+  data() {
+    return {
+    counter: 0
+    }
+  },
+  methods: {
+    increment(){
+      this.counter++
+    },
+    decrement(){
+      this.counter--
+    }
+  }
+}
+</script>
+<style>
+
+</style>
